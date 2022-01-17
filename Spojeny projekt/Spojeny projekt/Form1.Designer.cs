58c2e54fa742363257de6c3c5ae5b2061edc65ee
Mrnight020 <97515527+Mrnight020@users.noreply.github.com>
2022-01-17 09:00:57 +0100

    Add files via upload

---
+++ Spojeny projekt/Spojeny projekt/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Spojeny_projekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.vymazatlistbox = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Zadej celé kladné číslo";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkOrange;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(62, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 57);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Převod";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 220);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(88, 121);
+            this.listBox1.TabIndex = 3;
+            // 
+            // vymazatlistbox
+            // 
+            this.vymazatlistbox.BackColor = System.Drawing.Color.DarkOrange;
+            this.vymazatlistbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.vymazatlistbox.Location = new System.Drawing.Point(148, 220);
+            this.vymazatlistbox.Name = "vymazatlistbox";
+            this.vymazatlistbox.Size = new System.Drawing.Size(85, 121);
+            this.vymazatlistbox.TabIndex = 4;
+            this.vymazatlistbox.Text = "Vymazat listbox";
+            this.vymazatlistbox.UseVisualStyleBackColor = false;
+            this.vymazatlistbox.Click += new System.EventHandler(this.vymazatlistbox_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(10, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(228, 40);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Výpočet ciferného součtu a\r\n    dělitelů zadaného čísla";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(250, 353);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.vymazatlistbox);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button vymazatlistbox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
